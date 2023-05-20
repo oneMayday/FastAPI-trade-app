68108edca827c49c61dc793bd139f5ca6cda7e77
Pavel <pps.mayday@gmail.com>
2023-05-20 18:25:21 +0400

    Project structure refactoring

--- src/config.py
+++ src/config.py
@@ -1,10 +1,15 @@
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
+# DB parameters
 DB_USER = os.environ.get('DB_USER')
 DB_USER_PASSWORD = os.environ.get('DB_USER_PASSWORD')
 DB_NAME = os.environ.get('DB_NAME')
 DB_HOST = os.environ.get('DB_HOST')
 DB_PORT = os.environ.get('DB_PORT')
+
+# Auth parameters
+SECRET_AUTH = os.environ.get("SECRET_AUTH")
